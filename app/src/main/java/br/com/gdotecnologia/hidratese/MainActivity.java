cecc79afe48cddff580b381ee597506f57c1d4d7
GuiOrsolon <g.orsolon@gmail.com>
2022-02-16 21:04:46 -0300

    Commit Inicial do Projeto.

---
+++ app/src/main/java/br/com/gdotecnologia/hidratese/MainActivity.java
@@ -0,0 +1,38 @@
+package br.com.gdotecnologia.hidratese;
+
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, initializationStatus -> {
+
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+
+}
